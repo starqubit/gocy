b8bc115e735d5cc7c35db6d40851c5108bc207f7
dyb <dyb@slz-windows>
2022-01-27 11:24:21 +0800

    添加异常重试策略，避免服务器重启丢失数据，优化代码

--- R/client.go
+++ R/client.go
@@ -18,18 +18,22 @@ import (
 var r *server
 
 type server struct {
-	cache chan Message
-	url   string
-	name  string
+	cache   chan Message
+	retry   chan Message //异常需要重试的队列
+	url     string
+	name    string
+	errTime time.Time //异常时间
 }
 
 // 初始化服务
 func InitServer(name, url string) {
 	if r == nil {
 		r = &server{
-			name:  name,
-			url:   url,
-			cache: make(chan Message, 999999),
+			name:    name,
+			url:     url,
+			cache:   make(chan Message, 999999),
+			retry:   make(chan Message, 999999),
+			errTime: time.Now(),
 		}
 		go r.start()
 	}
@@ -37,6 +41,21 @@ func InitServer(name, url string) {
 
 // 启动服务
 func (s *server) start() {
+	go func() {
+		// 发送异常的数据重新加入发送队列
+		for {
+			m := <-s.retry
+			if time.Now().Unix()-s.errTime.Unix() <= 3 {
+				// 避免发生异常不断请求造成过大负载
+				time.Sleep(time.Second * 3)
+			}
+			s.cache <- m
+			if len(s.retry) > 10000 {
+				log.Println("异常日志队列数据超过10000，请检查接口是否异常")
+			}
+		}
+	}()
+	// 发送消息
 	for {
 		m := <-s.cache
 		s.post(m)
@@ -54,13 +73,14 @@ func (s *server) post(m Message) {
 	defer func() {
 		if err != nil {
 			log.Println(err)
-			// go func() {
-			// 	select {
-			// 	case s.cache <- m:
-			// 	case <-time.After(time.Millisecond * 100):
-			// 		return
-			// 	}
-			// }()
+			s.errTime = time.Now()
+			go func() {
+				select {
+				case s.retry <- m:
+				case <-time.After(time.Millisecond * 100):
+					return
+				}
+			}()
 		}
 	}()
 	client := resty.New()
@@ -68,7 +88,6 @@ func (s *server) post(m Message) {
 		"data": string(b),
 	}).Post(s.url)
 	if err != nil {
-		log.Println(err)
 		return
 	}
 	if resp.StatusCode() != 200 {
@@ -87,6 +106,10 @@ func (s *server) Output(level, flagId string, calldeep int, text string) {
 		file = "???"
 		line = 0
 	}
+	hostname, err := os.Hostname()
+	if err != nil || hostname == "" {
+		hostname = os.Getenv("HOSTNAME")
+	}
 	m := Message{
 		Timestamp: time.Now().UnixNano(),
 		Levelname: level,
@@ -96,7 +119,7 @@ func (s *server) Output(level, flagId string, calldeep int, text string) {
 		Weights:   1,
 		Name:      s.name,
 		FlagId:    flagId,
-		HostName:  os.Getenv("HOSTNAME"),
+		HostName:  hostname,
 	}
 	go func() {
 		select {
