2e546ee9750f7fc1957d450d29b297407c3470ea
dyb <605716308@qq.com>
2022-03-28 16:07:44 +0800

    添加N分钟前

--- common/date.go
+++ common/date.go
@@ -24,6 +24,8 @@ func Str2Time(strDate string) time.Time {
 		return t
 	} else if t, err := DateMonth2Time(strDate); err == nil {
 		return t
+	} else if t, err := BeforeMinute2Time(strDate); err == nil {
+		return t
 	}
 	log.Println("不支持的时间格式", strDate)
 	return time.Date(1971, 1, 1, 1, 1, 1, 1, time.Now().Location())
@@ -135,5 +137,26 @@ func DateMonth2Time(strDate string) (time.Time, error) {
 	}
 	now := time.Now()
 	dstTime := time.Date(now.Year(), time.Month(nMonth), nDay, now.Hour(), now.Minute(), now.Second(), 0, now.Location())
+	return dstTime, nil
+}
+
+// 日期转时间 6分钟前
+func BeforeMinute2Time(strDate string) (time.Time, error) {
+	tTime := time.Time{}
+	re, err := regexp.Compile(`^(\d{1,3})分钟前$`)
+	if err != nil {
+		return tTime, err
+	}
+	results := re.FindAllStringSubmatch(strDate, -1)
+	if len(results) == 0 {
+		return tTime, errors.New("匹配失败")
+	}
+
+	strOffset := results[0][1]
+	nOffset, err := strconv.Atoi(strOffset)
+	if err != nil {
+		return tTime, err
+	}
+	dstTime := time.Now().Add(-time.Minute * time.Duration(nOffset))
 	return dstTime, nil
 }
--- common/date_test.go
+++ common/date_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestDate(t *testing.T) {
 	t.Log("begin")
-	strDate := "2022年03月15日"
+	strDate := "5分钟前"
 	dstTime := Str2Time(strDate)
 	log.Println(dstTime.Format("2006-01-02 15:04:05"))
 }
