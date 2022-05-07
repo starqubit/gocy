6938c3a4fbfb456f906cd46bc059afa0dc5e0081
dyb <605716308@qq.com>
2022-05-07 15:10:02 +0800

    添加自定义Output

--- R/r_test.go
+++ R/r_test.go
@@ -17,6 +17,15 @@ func TestR(t *testing.T) {
 	time.Sleep(time.Second * 10)
 }
 
+func TestROutput(t *testing.T) {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	InitServer("R_test", "")
+	for i := 0; i < 5; i++ {
+		Output("DEBUG", "dddd", 1, "hhhhhahahaha")
+		time.Sleep(time.Second * 1)
+	}
+}
+
 func TestReportCustom(t *testing.T) {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	InitServer("R_test", "http://127.0.0.1:29721/report?token=123sdffd6t536")
